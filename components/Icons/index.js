f6d4fdc482a886be205e1c91da3015f6b724a80b
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-12 22:56:42 +0300

    icons vertical style added.

--- components/Icons/index.js
+++ components/Icons/index.js
@@ -1,8 +1,9 @@
 
 
-function Icons() {
+function Icons({vertical}) {
 	return (
-		<div className="icon_group">
+
+		<div className={ vertical ? "icons_vertical" : "icon_group"  }>
 			<a href="https://www.facebook.com/"><img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1615486678/brutalism/facebook_-_FontAwesome_h8bidl.png" alt="facebook icon"/></a>
 		<a href="https://instagram.com/"><img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1615486695/brutalism/social-instagram_-_simple-line-icons_wj2z4h.png" alt="instagram icon"/></a>
 		<a href="https://twitter.com/"><img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1615486668/brutalism/twitter_-_FontAwesome_wr3sle.png" alt="twitter icon"/>
