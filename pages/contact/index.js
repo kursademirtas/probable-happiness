b469932e6a9b057f29c15ca05476f676fca05309
Kursad Demirtas <69519700+kursademirtas@users.noreply.github.com>
2021-03-18 10:42:37 +0300

    contact page is done.

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,21 +2,21 @@ import React from 'react'
 import Map from '../../components/Map';
 import Icons from '../../components/Icons'
 import styles from './contact.module.css';
-
+import Layout from '../../components/Layout/Layout'
 const Contact = () => {
 
 	const containerStyle = {
 		width: '50vw',
-		height: '100vh'
+		height: '90vh'
 	  };
 
 	return (
-	
+	<Layout>
 		<div className={styles.contact_container}>
-			<img src="https://res.cloudinary.com/dlt51mt3f/image/upload/v1615466548/brutalism/contact_cisvdo.png" alt=""/>
-			<Map mapSize={containerStyle} />
+			
+			<div className={styles.map}><Map mapSize={containerStyle} /></div>
 			<aside className={styles.contact}>
-				<h2>Contact Us</h2>
+				<h2>Contact  <br/> Us</h2>
 				<div className={styles.contact_body}>
 					<span>Address</span>
 					<p>767 Fifth Ave.New York, NY 10153</p>
@@ -25,10 +25,12 @@ const Contact = () => {
 					<span>Phone</span>
 					<p>+123 456 789</p>
 					<span>Follow us</span>
-
+					<div ><Icons /></div>
 				</div>
 			</aside>
 		</div>
+	</Layout>
+	
 
 	)
 }
